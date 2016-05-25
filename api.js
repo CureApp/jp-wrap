1bfc819eed973a6f177c3ef51fffe0c07cb1b9aa
CircleCI <circleci@cureapp.jp>
2016-05-25 05:16:38 +0000

    gh-pages

---
+++ api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "JpWrap",
+        "Word"
+    ],
+    "modules": [
+        "jp-wrap"
+    ],
+    "allModules": [
+        {
+            "displayName": "jp-wrap",
+            "name": "jp-wrap",
+            "description": "日本語の禁則処理を行って改行する"
+        }
+    ],
+    "elements": []
+} };
+});
